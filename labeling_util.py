0d7425fb5aeeac4e9d0c84c148d6697989511393
idanacumen <idan@acumen.io>
2020-05-12 16:28:17 +0300

    added types to labeling utils and the label_dataset utility

--- labeling_util.py
+++ labeling_util.py
@@ -1,10 +1,30 @@
+import pandas as pd
 
-def get_false_positives(df
-                        , classifier
-                        , concept):
-    return df[(df[classifier] == True) & (df[concept] == False)]
 
-def get_false_negatives(df
+def label_dataset(df : pd.DataFrame
                         , classifier
-                        , concept):
-    return df[(df[classifier] == False) & (df[concept] == True)]
+                        , features
+                        , classifier_column : str) -> pd.DataFrame:
+    """
+
+    :param df:
+    :param classifier:
+    :param features: Some other features, keys, concept should be kept but not used.
+    These are the feature to use in prediction.
+    :param classifier_column:
+    :return:
+    """
+
+    df[classifier_column] = classifier.predict(df[features])
+
+    return df
+
+def get_false_positives(df : pd.DataFrame
+                        , classifier_column : str
+                        , concept_column : str) -> pd.DataFrame:
+    return df[(df[classifier_column] == True) & (df[concept_column] == False)]
+
+def get_false_negatives(df : pd.DataFrame
+                        , classifier_column : str
+                        , concept_column : str) -> pd.DataFrame:
+    return df[(df[classifier_column] == False) & (df[concept_column] == True)]
