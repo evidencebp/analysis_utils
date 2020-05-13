7ab84f2b5e8b5566002be8948ccbf4b02872e3af
idanacumen <idan@acumen.io>
2020-05-13 08:50:56 +0300

    Add utilitins to collect labels

--- labeling_util.py
+++ labeling_util.py
@@ -1,5 +1,7 @@
 import pandas as pd
 
+from typing import List
+string_list = List[str]
 
 def label_dataset(df : pd.DataFrame
                         , classifier
@@ -28,3 +30,33 @@ def get_false_negatives(df : pd.DataFrame
                         , classifier_column : str
                         , concept_column : str) -> pd.DataFrame:
     return df[(df[classifier_column] == False) & (df[concept_column] == True)]
+
+def get_labeling_df(keys :string_list
+                    , concept : str) -> pd.DataFrame :
+    """
+    Create a new labeling data frame
+    :return:
+    """
+
+    df = pd.DataFrame(columns=keys + [concept] + ['comment'])
+
+    return df
+
+def label_sample(samples_df : pd.DataFrame
+                  , sample_position : int
+                  , labels_df : pd.DataFrame
+                  , comment : str
+                  , keys :string_list
+                  , concept : str) -> pd.DataFrame :
+
+    new_dict = {}
+    for i in keys:
+        new_dict[i] = samples_df.iloc[sample_position][i]
+
+    new_dict[concept] = samples_df.iloc[sample_position][concept]
+    new_dict['comment'] = comment
+
+    labels_df = labels_df.append(new_dict, ignore_index=True)
+    labels_df = labels_df.drop_duplicates()
+
+    return labels_df
