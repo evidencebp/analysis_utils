4b9f0d1fc647e8d43db45855688b6a23e91de9cf
idanacumen <idan@acumen.io>
2020-05-19 11:37:51 +0300

    Refactor plot cdf, added a parameter to replace underscores to spaces

--- plot_cdf.py
+++ plot_cdf.py
@@ -10,7 +10,8 @@ def plot_cdf(df : pd.DataFrame
                 , column_name : str
                 , title : str
                 , output_file : str = None
-                , subsets =[]):
+                , subsets =[]
+                , underscore_to_space=True):
 
     data = []
 
@@ -19,14 +20,26 @@ def plot_cdf(df : pd.DataFrame
     cdf = cdf.reset_index()
     cdf.columns = [column_name, 'cdf']
 
+    if underscore_to_space:
+        column_to_present = column_name.replace("_", " ")
+        title_to_present = title.replace("_", " ")
+    else:
+        column_to_present = column_name
+        title_to_present = title
+
     data.append(go.Scatter(
         x=cdf[column_name],
         y=cdf.cdf,
         mode='lines',
-        name=column_name
+        name=column_to_present
     ))
 
     for i in subsets:
+        if underscore_to_space:
+            val_to_present = str(i['value']).replace("_", " ")
+        else:
+            val_to_present = str(i['value'])
+
         cdf = df[df[i['column']] == i['value']][column_name].value_counts(normalize=True).sort_index().cumsum()
         cdf = pd.DataFrame(cdf)
         cdf = cdf.reset_index()
@@ -36,11 +49,11 @@ def plot_cdf(df : pd.DataFrame
             x=cdf[column_name],
             y=cdf.cdf,
             mode='lines',
-            name=str(i['value'])
+            name=val_to_present
         ))
 
     layout = go.Layout(
-        title=title,
+        title=title_to_present,
         xaxis=dict(
             title='CCP',
             titlefont=dict(
@@ -50,7 +63,7 @@ def plot_cdf(df : pd.DataFrame
             )
         ),
         yaxis=dict(
-            title='CDF of ' + column_name,
+            title='CDF of ' + column_to_present,
             titlefont=dict(
                 family='Courier New, monospace',
                 size=24,
