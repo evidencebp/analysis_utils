d3e7c4e34e9729ad4cd9d3e8d9ef164a0e9f3d3b
idanacumen <idan@acumen.io>
2020-05-20 15:30:45 +0300

    Extended plot cdf - weight and limit

--- plot_cdf.py
+++ plot_cdf.py
@@ -11,11 +11,28 @@ def plot_cdf(df : pd.DataFrame
                 , title : str
                 , output_file : str = None
                 , subsets =[]
-                , underscore_to_space=True):
+                , weight_column=None
+                , underscore_to_space=True
+                , limit=None):
 
+    auxiliry_col = 'prob'
     data = []
 
-    cdf = df[column_name].value_counts(normalize=True).sort_index().cumsum()
+    local = df.copy()
+    if limit:
+        local = local[local[column_name] <= limit]
+
+    if weight_column:
+        if subsets:
+            g= local.groupby(column_name).agg({weight_column : 'sum'})
+        else:
+            g = local
+        weight_sum = g[weight_column].sum()
+        g[auxiliry_col] = g[weight_column] / weight_sum
+        cdf = g[auxiliry_col].sort_index().cumsum()
+    else:
+        cdf = local[column_name].value_counts(normalize=True).sort_index().cumsum()
+
     cdf = pd.DataFrame(cdf)
     cdf = cdf.reset_index()
     cdf.columns = [column_name, 'cdf']
@@ -40,7 +57,17 @@ def plot_cdf(df : pd.DataFrame
         else:
             val_to_present = str(i['value'])
 
-        cdf = df[df[i['column']] == i['value']][column_name].value_counts(normalize=True).sort_index().cumsum()
+        if weight_column:
+            if subsets:
+                g = local[local[i['column']] == i['value']].groupby([column_name, i['column']], as_index=False).agg({weight_column: 'sum'})
+            else:
+                g = local[local[i['column']] == i['value']]
+            weight_sum = g[weight_column].sum()
+            g[auxiliry_col] = g[weight_column] / weight_sum
+            cdf = g[auxiliry_col].sort_index().cumsum()
+        else:
+            cdf = local[local[i['column']] == i['value']][column_name].value_counts(normalize=True).sort_index().cumsum()
+
         cdf = pd.DataFrame(cdf)
         cdf = cdf.reset_index()
         cdf.columns = [column_name, 'cdf']
@@ -78,12 +105,17 @@ def plot_cdf(df : pd.DataFrame
     if output_file:
         fig.write_image(output_file)
 
+    return fig
+
 
 def plot_cdf_by_column(df : pd.DataFrame
                 , column_name : str
                 , title : str
                 , output_file : str
-                , subsets_column : str):
+                , subsets_column : str
+                , weight_column=None
+                , underscore_to_space=True
+                , limit=None):
     subsets = []
     subset_values = df[subsets_column].unique()
 
@@ -91,9 +123,12 @@ def plot_cdf_by_column(df : pd.DataFrame
         subsets.append({'column' :subsets_column
                            , 'value' : i})
 
-    plot_cdf(df
-        , column_name
-        , title
-        , output_file
-        , subsets)
+    plot_cdf(df=df
+        , column_name=column_name
+        , title=title
+        , output_file=output_file
+        , subsets=subsets
+        , weight_column=weight_column
+        , underscore_to_space=underscore_to_space
+        , limit=limit)
 
