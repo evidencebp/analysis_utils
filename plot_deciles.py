8a23285318af79b75fee04b60fa9acabbf37d980
idanacumen <idan@acumen.io>
2020-05-31 15:37:05 +0300

    Added plot deciles

---
+++ plot_deciles.py
@@ -0,0 +1,80 @@
+from numpy import inf
+import pandas as pd
+import plotly.graph_objs as go
+
+from feature_pair_analysis import bin_metric_by_quantiles
+
+
+def plot_deciles(df
+                 , grouping_column
+                 , metric_column
+                 , title
+                 , xaxis_title
+                 , output_file=None):
+    deciles_column = grouping_column + '_deciles'
+    deciles_df = generate_deciles(df=df
+                                  , grouping_column=grouping_column
+                                  , deciles_column=deciles_column)
+    deciles_df = deciles_df.sort_values(deciles_column)
+    return plot_groups(deciles_df
+                 , grouping_column=deciles_column
+                 , metric_column=metric_column
+                 , title=title
+                 , xaxis_title=xaxis_title
+                 , output_file=output_file)
+
+def generate_deciles(df : pd.DataFrame
+                     , grouping_column : str
+                     , deciles_column: str
+                     , top_val=inf):
+    return bin_metric_by_quantiles(df
+                                   , grouping_column
+                                   , deciles_column
+                                   , bins=10
+                                   , top_val=top_val
+                                   )
+
+
+def plot_groups(df
+                 , grouping_column
+                 , metric_column
+                 , title
+                 , xaxis_title
+                 , output_file=None):
+
+    fig = go.Figure()
+    item = 0;
+    for cur_group in df[grouping_column].unique():
+        color = 'blue' # 'rgb(' +str(20*item +1) + ',' + str(100 + 10*item) +',' + str(254 - 15*item) + ')'
+        if str(cur_group).find(',') > 0:
+            name = '<=' + str(cur_group)[str(cur_group).find(',')+1:-1]
+        else:
+            name = str(cur_group)
+        trace = go.Box(
+            y=df[df[grouping_column] == cur_group][metric_column].tolist(),
+            name=name,
+            marker=dict(
+                color=color,
+            ),
+            boxpoints=False,
+            boxmean=True
+        )
+        fig.add_trace(trace)
+        item += 1
+
+    fig.update_layout(
+        title=title.replace("_", " "),
+        xaxis_title=xaxis_title.replace("_", " "),
+        yaxis_title=metric_column.replace("_", " "),
+        font=dict(
+            family="Courier New, monospace",
+            size=18,
+            color="black"
+        )
+    )
+    fig.update_layout(showlegend=False)
+
+    if output_file:
+        fig.write_image(output_file)
+
+    return fig
