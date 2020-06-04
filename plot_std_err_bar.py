82dc868e88caf9ba4ee5fdf23f76c1e7235d368c
idanacumen <idan@acumen.io>
2020-06-04 17:25:45 +0300

    Added grouping to plot_std_err_bar

--- plot_std_err_bar.py
+++ plot_std_err_bar.py
@@ -9,23 +9,44 @@ def plot_std_err_bar(df : pd.DataFrame
                      , y_count: str
                      , title: str
                      , x_title: str
-                     , y_title: str):
-    x = df[x_column]
-    y = df[y_avg]
-    error = df.apply(lambda x: x[y_std] / sqrt(x[y_count])
-                     , axis=1)
+                     , y_title: str
+                     , grouping_column: str = None):
+    if grouping_column:
+        grouping_values = df[grouping_column].unique()
+        data = []
+        for i in grouping_values:
+            x = df[df[grouping_column] == i][x_column]
+            y = df[df[grouping_column] == i][y_avg]
+            error = df.apply(lambda x: x[y_std] / sqrt(x[y_count])
+                             , axis=1)
+
+            data.append(go.Bar(
+                x=x,
+                y=y,
+                name=i,
+                error_y=dict(
+                    type='data',
+                    array=error,
+                    visible=True
+                )))
+
+    else:
+        x = df[x_column]
+        y = df[y_avg]
+        error = df.apply(lambda x: x[y_std] / sqrt(x[y_count])
+                         , axis=1)
 
 
-    data = [go.Bar(
-        x=x,
-        y=y,
-        name=title,
-        error_y=dict(
-            type='data',
-            array=error,
-            visible=True
-        )
-    )]
+        data = [go.Bar(
+            x=x,
+            y=y,
+            name=title,
+            error_y=dict(
+                type='data',
+                array=error,
+                visible=True
+            )
+        )]
 
 
     layout = go.Layout(
@@ -49,6 +70,7 @@ def plot_std_err_bar(df : pd.DataFrame
             )
         )
     )
-    fig = go.Figure(data=data, layout=layout)
+    fig = go.Figure(data=data
+                    , layout=layout)
 
     return fig
