9eba7b5c111e3000cc4e9a34e3eac531def1397c
idanacumen <idan@acumen.io>
2020-06-17 09:13:12 +0300

    Added error logging to batch_process

--- batch_process.py
+++ batch_process.py
@@ -6,10 +6,12 @@ def batch_process(input_file : str
                    , prev_file : str
                    , fetch_function
                    , keys
+                   , error_file : str = None
                    , merge_with_prev=True
                    , batch_size=100):
 
     outputs_list = []
+    errors_list = []
     df = pd.read_csv(input_file)
 
     if prev_file:
@@ -26,11 +28,14 @@ def batch_process(input_file : str
     item = 0;
 
     for _, i in df.iterrows():
-        sample_output = fetch_function(i)
-        record = []
-        for k in keys:
-            record.append(i[k])
+        record = get_item_keys(i, keys_columns=keys)
+        try:
+            sample_output = fetch_function(i)
+        except:
+            if error_file:
+                errors_list.append(record)
         record.append(sample_output)
+
         outputs_list.append(record)
         item += 1
         if item % batch_size == 0:
@@ -39,6 +44,13 @@ def batch_process(input_file : str
                 output_df.columns = keys + ['output']
 
             output_df.to_csv(output_file, index=False)
+
+            if error_file and len(output_df):
+                errors_df = pd.DataFrame(errors_list)
+                errors_df.columns = keys
+                errors_df.to_csv(error_file, index=False)
+
+
             print("Processed {items} items, time {time}".format(items=item
                                                                 , time=datetime.datetime.now()))
 
@@ -46,8 +58,22 @@ def batch_process(input_file : str
     if len(output_df):
         output_df.columns = keys + ['output']
 
+    if error_file and len(output_df):
+        errors_df = pd.DataFrame(errors_list)
+        errors_df.columns = keys
+        errors_df.to_csv(error_file, index=False)
+
     if merge_with_prev and prev_file :
         output_df = pd.concat([prev_df, output_df])
     output_df.to_csv(output_file, index=False)
 
     return output_df
+
+
+def get_item_keys(i, keys_columns):
+    record = []
+    for k in keys_columns:
+        record.append(i[k])
+
+
+    return record
