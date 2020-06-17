629d271291b3a6d2be81596e299cad6c41f31d76
idanacumen <idan@acumen.io>
2020-06-17 10:45:19 +0300

    Refactor batch_process into a class

--- batch_process.py
+++ batch_process.py
@@ -1,79 +1,112 @@
 import pandas as pd
 import datetime
 
-def batch_process(input_file : str
+class BatchProcessor:
+    def __init__(self
+                   , input_file : str
                    , output_file : str
                    , prev_file : str
                    , fetch_function
                    , keys
                    , error_file : str = None
                    , merge_with_prev=True
-                   , batch_size=100):
+                   , batch_size=100
+                   , output_column='output'):
 
-    outputs_list = []
-    errors_list = []
-    df = pd.read_csv(input_file)
-
-    if prev_file:
-        # See https://stackoverflow.com/questions/28901683/pandas-get-rows-which-are-not-in-other-dataframe
-        pandas_merge_column = '_merge'
-        prev_df = pd.read_csv(prev_file)
-        prev_df = prev_df[keys]
-        joint = pd.merge(df, prev_df, on=keys, how='left', indicator=True)
-        df = joint[joint[pandas_merge_column] == 'left_only']
-        df = df.drop(columns=[pandas_merge_column])
-
-    print("About to process {items} items, time {time}".format(items=len(df)
-                                                                , time=datetime.datetime.now()))
-    item = 0;
-
-    for _, i in df.iterrows():
-        record = get_item_keys(i, keys_columns=keys)
-        try:
-            sample_output = fetch_function(i)
-        except:
-            if error_file:
-                errors_list.append(record)
-        record.append(sample_output)
-
-        outputs_list.append(record)
-        item += 1
-        if item % batch_size == 0:
-            output_df = pd.DataFrame(outputs_list)
-            if len(output_df):
-                output_df.columns = keys + ['output']
-
-            output_df.to_csv(output_file, index=False)
-
-            if error_file and len(output_df):
-                errors_df = pd.DataFrame(errors_list)
-                errors_df.columns = keys
-                errors_df.to_csv(error_file, index=False)
+        self.input_file = input_file
+        self.output_file = output_file
+        self.prev_file = prev_file
+        self.fetch_function = fetch_function
+        self.keys = keys
+        self.error_file = error_file
+        self.merge_with_prev = merge_with_prev
+        self.batch_size = batch_size
+        self.output_column = output_column
 
 
-            print("Processed {items} items, time {time}".format(items=item
-                                                                , time=datetime.datetime.now()))
+    def process(self):
 
-    output_df = pd.DataFrame(outputs_list)
-    if len(output_df):
-        output_df.columns = keys + ['output']
+        outputs_list = []
+        errors_list = []
+        df = pd.read_csv(self.input_file)
 
-    if error_file and len(output_df):
-        errors_df = pd.DataFrame(errors_list)
-        errors_df.columns = keys
-        errors_df.to_csv(error_file, index=False)
+        if self.prev_file:
+            # See https://stackoverflow.com/questions/28901683/pandas-get-rows-which-are-not-in-other-dataframe
+            pandas_merge_column = '_merge'
+            prev_df = pd.read_csv(self.prev_file)
+            prev_df = prev_df[self.keys]
+            joint = pd.merge(df
+                             , prev_df
+                             , on=self.keys
+                             , how='left'
+                             , indicator=True)
+            df = joint[joint[pandas_merge_column] == 'left_only']
+            df = df.drop(columns=[pandas_merge_column])
 
-    if merge_with_prev and prev_file :
-        output_df = pd.concat([prev_df, output_df])
-    output_df.to_csv(output_file, index=False)
+        print("About to process {items} items, time {time}".format(items=len(df)
+                                                                    , time=datetime.datetime.now()))
+        item = 0;
 
-    return output_df
+        for _, i in df.iterrows():
+            record = BatchProcessor.get_item_keys(i
+                                                  , keys_columns=self.keys)
+            try:
+                sample_output = self.fetch_function(i)
+            except:
+                if self.error_file:
+                    errors_list.append(record)
+            record.append(sample_output)
 
+            outputs_list.append(record)
+            item += 1
+            if item % self.batch_size == 0:
+                self.write_results(errors_list
+                                   , outputs_list)
 
-def get_item_keys(i, keys_columns):
-    record = []
-    for k in keys_columns:
-        record.append(i[k])
+                print("Processed {items} items, time {time}".format(items=item
+                                                                    , time=datetime.datetime.now()))
 
+        output_df = pd.DataFrame(outputs_list)
+        if len(output_df):
+            output_df.columns = self.keys + [self.output_column]
 
-    return record
+        self.write_results(errors_list
+                           , outputs_list)
+
+        if self.merge_with_prev and self.prev_file :
+            output_df = pd.concat([prev_df
+                                      , output_df])
+        output_df.to_csv(self.output_file
+                         , index=False)
+
+        return output_df
+
+    def write_results(self
+                      , errors_list
+                      , outputs_list):
+        self.list_to_file(outputs_list
+                          , columns=self.keys + [self.output_column]
+                          , output_file=self.output_file)
+        if self.error_file and len(errors_list):
+            self.list_to_file(errors_list
+                              , columns=self.keys
+                              , output_file=self.error_file)
+
+    def list_to_file(self
+                     , outputs_list
+                     , columns
+                     , output_file):
+        output_df = pd.DataFrame(outputs_list)
+        if len(output_df):
+            output_df.columns = columns
+        output_df.to_csv(output_file
+                         , index=False)
+        return output_df
+
+    @staticmethod
+    def get_item_keys(i, keys_columns):
+        record = []
+        for k in keys_columns:
+            record.append(i[k])
+
+        return record
