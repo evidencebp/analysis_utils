affe3925b332c920bd5aa25ad645ca349ecca57a
idanacumen <idan@acumen.io>
2020-07-08 20:29:01 +0300

    Extend twins analysis with filtering function

--- compare_twin_behaviours.py
+++ compare_twin_behaviours.py
@@ -1,18 +1,28 @@
 import pandas as pd
 
+COMPARISON_SUFFIX = '_cmp'
+
+
 def compare_twin_behaviours(first_behaviour : pd.DataFrame
                             , second_behaviour : pd.DataFrame
                             , keys : list
                             , comparision_columns : list
-                            , comparision_function) -> pd.DataFrame:
+                            , comparision_function
+                            , filtering_function = None) -> pd.DataFrame:
 
     first_to_match = first_behaviour[keys + comparision_columns].copy()
     second_to_match = second_behaviour[keys + comparision_columns].copy()
     joint = pd.merge(first_to_match, second_to_match, on=keys)
 
+    if filtering_function:
+        filter_column = 'filter'
+        joint[filter_column] = joint.apply(filtering_function, axis=1)
+        joint = joint[~joint[filter_column]]
+        joint = joint.drop(columns=[filter_column])
+
     joint_cols = keys.copy()
     for i in comparision_columns:
-        key = i + '_cmp'
+        key = i + COMPARISON_SUFFIX
         joint_cols.append(key)
         joint[key] = joint.apply(lambda x : comparision_function(x[i +'_x'], x[i +'_y'])
                                 ,axis=1)
