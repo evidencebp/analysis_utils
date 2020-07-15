23d0eb4d506bf7a151afa7cf99e9c57590bbdfbb
idanacumen <idan@acumen.io>
2020-07-15 13:09:25 +0300

    Added number of difference to twin comparision

--- compare_twin_behaviours.py
+++ compare_twin_behaviours.py
@@ -8,7 +8,8 @@ def compare_twin_behaviours(first_behaviour : pd.DataFrame
                             , keys : list
                             , comparision_columns : list
                             , comparision_function
-                            , filtering_function = None) -> pd.DataFrame:
+                            , filtering_function = None
+                            , differences_num : str = None) -> pd.DataFrame:
 
     first_to_match = first_behaviour[keys + comparision_columns].copy()
     second_to_match = second_behaviour[keys + comparision_columns].copy()
@@ -27,5 +28,11 @@ def compare_twin_behaviours(first_behaviour : pd.DataFrame
         joint[key] = joint.apply(lambda x : comparision_function(x[i +'_x'], x[i +'_y'])
                                 ,axis=1)
 
-    return joint[joint_cols]
+    if differences_num:
+        joint[differences_num] = joint[[i + COMPARISON_SUFFIX for i in comparision_columns]].sum(axis=1)
+        relevant_columns = joint_cols + [differences_num]
+    else:
+        relevant_columns = joint_cols
+
+    return joint[relevant_columns]
 
