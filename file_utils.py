535442fc3d7aad6345dee9c7c4344a565f8e41f1
idanacumen <idan@acumen.io>
2020-08-27 09:57:53 +0300

    Added merge_csv_directory

--- file_utils.py
+++ file_utils.py
@@ -2,6 +2,8 @@
 Utilities for general file handling
 
 """
+import os
+
 from typing import List
 
 import pandas as pd
@@ -21,5 +23,22 @@ def merge_csv_files(files_list: Strings) -> pd.DataFrame:
         data_frames.append(df)
 
     joint = pd.concat(data_frames)
+
+    return joint
+
+
+def merge_csv_directory(files_directory: str
+                        , verify_extension: bool=True) -> pd.DataFrame:
+    """
+
+    :param files_directory:
+    :param verify_extension:
+    :return:
+    """
+    files_list = [os.path.join(files_directory, file) for file in os.listdir(files_directory)]
+    if verify_extension:
+        files_list = [file for file in files_list if file.endswith(".csv") ]
+
+    joint = merge_csv_files(files_list)
 
     return joint
