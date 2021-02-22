674c6ce46de9c89873cc3170a14c4da8bc41e22f
idanacumen <idan@acumen.io>
2021-02-22 14:52:53 +0200

    Added tests for confidence_entropy

--- test_maximize_classifiers_agreement.py
+++ test_maximize_classifiers_agreement.py
@@ -3,7 +3,7 @@ from numpy.testing import assert_array_equal
 import pandas as pd
 import pytest
 
-from maximize_classifiers_agreement import minimize_classifiers_entropy, FixedLogisticRegression
+from maximize_classifiers_agreement import minimize_classifiers_entropy, FixedLogisticRegression, confidence_entropy
 
 def equal_dictionaries(dict1
                        , dict2):
@@ -97,12 +97,12 @@ pytest.param(
 
                          ])
 def test_minimize_classifiers_entropy(X
-                                        , step_size
-                                        , min_val
-                                        , max_val
-                                        , digits
-                                        , include_zero
-                                        , expected):
+                                       , step_size
+                                       , min_val
+                                       , max_val
+                                       , digits
+                                       , include_zero
+                                       , expected):
 
     actual = minimize_classifiers_entropy(X=X
                                  , step_size=step_size
@@ -150,3 +150,35 @@ def test_minimize_classifiers_entropy(X
 
      , id='duplicated_f1_and_minus_f1')
 """
+
+
+@pytest.mark.parametrize(('y'
+                            , 'y_pred'
+                            , 'expected')
+, [
+pytest.param(
+            [None]
+            , [0.5]
+            , 1.0
+, id='reg1')
+, pytest.param(
+            [None]
+            , [0.5, 0.5]
+            , 2.0
+, id='reg2')
+, pytest.param(
+            [None]
+            , [0.5, 0.5, 1]
+            , 2.0
+, id='reg3')
+
+
+                         ])
+def test_confidence_entropy(y
+                            , y_pred
+                            , expected):
+
+    actual = confidence_entropy(y, y_pred)
+
+    assert actual == expected
+
