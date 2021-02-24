83cd4ae53d07c5ee5659dc2337318dfdbcb31768
idanacumen <idan@acumen.io>
2021-02-24 13:38:19 +0200

    Added side_binning_by_direction

--- binning_utils.py
+++ binning_utils.py
@@ -1,18 +1,22 @@
 """
-Utilities ofr values binning
+Utilities for values binning
 
 """
 
 import pandas as pd
+
+from cochange_analysis import the_lower_the_better
+
+SIDES_SUFFIX = '_SIDES'
 
 def sides_binning(df: pd.DataFrame
                   , column:str
                   , labels=[0,1,2]):
     """
         Bins values in to the lower 25%, higher 25% and the middle
-    :param df:
-    :param column:
-    :param labels:
+    :param df: Dataframe to aggregate
+    :param column: Columns to aggregate
+    :param labels: The labels to assign to each group
     :return:
     """
     quantiles = [ .25, 0.75, 1]
@@ -30,3 +34,35 @@ def sides_binning(df: pd.DataFrame
     return pd.cut(df[column],
             bins=quantiles_vals,
             labels=labels)
+
+def side_binning_by_direction(df: pd.DataFrame
+                            , metrics
+                            , labels=['Bad', 'Medium', 'Good'] # values should be ordered from bad to good.
+                            , suffix=SIDES_SUFFIX):
+    """
+
+    :param df: The data frame with the row values of the metrics
+    :param metrics: A dictionary with a "direction" function of the metric
+            - the lower the better of the higher the better
+    :param labels: Labels to assign to the groups. Values should be the same to all metric by their semantic meaning.
+                   Hence, for the higher the better, high values should get good, the last value.
+                   For the lower the better, high values should get bad, the first value.
+    :param suffix: A suffix of the new tables.
+    :return:
+    """
+
+    for i in metrics:
+
+        cur_col = i + suffix
+        # Checking that the metric is better when lower
+        if (the_lower_the_better(1,0) == metrics[i](1,0)):
+            metric_labels = labels[::-1]
+        else:
+            metric_labels = labels
+
+        df[cur_col] = sides_binning(df=df
+                                        , column=i
+                                        , labels=metric_labels)
+        df[cur_col] = df[cur_col].astype(str)
+
+    return df
--- test_binning_utils.py
+++ test_binning_utils.py
@@ -1,9 +1,10 @@
 import numpy as np
 import pandas as pd
-from pandas.testing import assert_series_equal
+from pandas.testing import assert_frame_equal
 import pytest
 
-from binning_utils import sides_binning
+from binning_utils import sides_binning, side_binning_by_direction, SIDES_SUFFIX
+from cochange_analysis import the_lower_the_better, the_higher_the_better
 
 @pytest.mark.parametrize(('df'
                   , 'column'
@@ -51,4 +52,42 @@ def test_sides_binning(df: pd.DataFrame
 
     assert actual.tolist() == expected.tolist()
 
+
+@pytest.mark.parametrize(('df'
+                  , 'metrics'
+                  , 'suffix'
+                  , 'labels'
+                  , 'expected')
+    , [
+pytest.param(
+            pd.DataFrame([
+                [1, 1]
+                , [2, 2]
+                , [3, 3]
+                , [4, 4]
+            ], columns=['lower_better', 'higher_better'])
+            , {'lower_better' : the_lower_the_better, 'higher_better' : the_higher_the_better}
+            , SIDES_SUFFIX
+            , ['Bad', 'Medium', 'Good']
+            , pd.DataFrame([
+                [1, 1, 'Good', 'Bad']
+                , [2, 2, 'Medium', 'Medium']
+                , [3, 3, 'Medium', 'Medium']
+                , [4, 4, 'Bad', 'Good']
+            ], columns=['lower_better', 'higher_better', 'lower_better_SIDES', 'higher_better_SIDES'])
+
+, id='reg1')
+                         ])
+def test_side_binning_by_direction(df: pd.DataFrame
+                  , metrics
+                  , suffix
+                  , labels
+                  , expected):
+
+    actual = side_binning_by_direction(df=df
+                            , metrics=metrics
+                            , suffix=suffix
+                            , labels=labels)
+
+    assert_frame_equal(actual, expected)
 
