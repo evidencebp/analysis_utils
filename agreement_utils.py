75b73e571a1c0fb8ae8b48adda1df6219c32cd86
idanacumen <idan@acumen.io>
2021-03-01 10:37:44 +0200

    Added agreement_utils

---
+++ agreement_utils.py
@@ -0,0 +1,84 @@
+"""
+A utility to compute between concepts and metric.
+For each metric, the agreement is computed with each concept.
+
+The basic definition of agreement is accuracy.
+That works well for categorial values.
+
+For continuous values we use side binning first, yet other categorizations are also ok.
+
+"""
+
+import pandas as pd
+from sklearn.metrics import accuracy_score
+
+from binning_utils import sides_binning, side_binning_by_direction, SIDES_SUFFIX
+
+
+def compute_agreement(df: pd.DataFrame
+                       , concepts
+                       , metrics):
+    """
+    Computes the accuracy between concepts and metrics.
+
+    :param df: The data frame containing the metrics and concepts
+    :param concepts: A *dictionary* of concepts. Each concept dictionary value is a direction function
+                    (the_lower_the_better/the_higher_the_better).
+    :param metrics: A *dictionary* of metrics. Each metric dictionary value is a direction function
+                    (the_lower_the_better/the_higher_the_better).
+                    The metrics can include the concepts.
+    :return: A data frame of the accuracy of concepts with respect to metrics.
+    """
+
+    rows = []
+
+    for metric in metrics:
+
+        row = [metric]
+
+        for concept in concepts:
+            agreement = accuracy_score(df[metric]
+                                       , df[concept])
+            row.append(agreement)
+
+        rows.append(row)
+
+    agreement_df = pd.DataFrame(rows
+                               , columns=['metric'] + concepts).sort_values('metric')
+
+    return agreement_df
+
+
+def agreement_with_concepts(df
+                          , concepts
+                          , metrics
+                          , stats_file=None
+                          , suffix=SIDES_SUFFIX):
+    """
+    Does sides binning for categorial concepts and compute accuracy on them.
+
+    :param df: The data frame containing the metrics and concepts
+    :param concepts: A *dictionary* of concepts. Each concept dictionary value is a direction function
+                    (the_lower_the_better/the_higher_the_better).
+    :param metrics: A *dictionary* of metrics. Each metric dictionary value is a direction function
+                    (the_lower_the_better/the_higher_the_better).
+                    The metrics can include the concepts.
+    :return: A data frame of the accuracy of concepts with respect to metrics.
+
+    :param stats_file: Location to store the result, None if not needed.
+    :param suffix: A suffix for the side_binning resulting columns.
+    :return: A data frame of the accuracy of concepts with respect to metrics, on the side_binning values.
+    """
+
+    df = side_binning_by_direction(df
+                            , metrics=metrics
+                            , suffix=suffix)
+
+    agreement_df = compute_agreement(df
+                       , [i + suffix for i in concepts.keys()]
+                       , [i + suffix for i in metrics.keys()])
+    if stats_file:
+        agreement_df.to_csv(stats_file
+                            , index=False)
+
+    return agreement_df
---
+++ test_agreement_utils.py
@@ -0,0 +1,79 @@
+import numpy as np
+import pandas as pd
+from pandas.testing import assert_frame_equal
+import pytest
+
+from agreement_utils import compute_agreement, agreement_with_concepts
+from binning_utils import sides_binning, side_binning_by_direction, SIDES_SUFFIX
+from cochange_analysis import the_lower_the_better, the_higher_the_better
+
+@pytest.mark.parametrize(('df'
+                            , 'concepts'
+                            , 'metrics'
+                            , 'expected')
+    , [
+pytest.param(
+            pd.DataFrame([
+                [1, 0, 1, 0]
+                , [1, 0, 1, 0]
+                , [0, 1, 0, 1]
+                , [0, 1, 0, 1]
+            ], columns=['c1', 'c2', 'm1', 'm2'])
+            , ['c1', 'c2']
+            , ['m1', 'm2']
+            , pd.DataFrame([
+                ['m1', 1.0, 0.0]
+                , ['m2', 0.0, 1.0]
+    ], columns=['metric', 'c1', 'c2'])
+
+, id='reg1')
+                         ])
+def test_compute_agreement(df: pd.DataFrame
+                            , concepts
+                            , metrics
+                            , expected):
+
+    actual = compute_agreement(df=df
+                       , concepts=concepts
+                       , metrics=metrics)
+
+    assert_frame_equal(actual, expected)
+
+
+@pytest.mark.parametrize(('df'
+                            , 'concepts'
+                            , 'metrics'
+                            , 'expected')
+    , [
+pytest.param(
+            pd.DataFrame([
+                [1, 4, 1, 4]
+                , [2, 3, 2, 3]
+                , [3, 2, 3, 2]
+                , [4, 1, 4, 1]
+            ], columns=['c1', 'c2', 'm1', 'm2'])
+            , {'c1' : the_lower_the_better
+                    , 'c2' : the_lower_the_better}
+            , {'c1' : the_lower_the_better
+                    , 'c2' : the_lower_the_better
+                    , 'm1' : the_lower_the_better
+                    , 'm2' : the_lower_the_better}
+            , pd.DataFrame([
+                            ['c1_SIDES', 1.0, 0.5]
+                            , ['c2_SIDES', 0.5, 1.0]
+                            , ['m1_SIDES', 1.0, 0.5]
+                            , ['m2_SIDES', 0.5, 1.0]
+    ], columns=['metric', 'c1_SIDES', 'c2_SIDES'])
+
+, id='reg1')
+                         ])
+def test_agreement_with_concepts(df: pd.DataFrame
+                            , concepts
+                            , metrics
+                            , expected):
+
+    actual = agreement_with_concepts(df
+                          , concepts
+                          , metrics)
+
+    assert_frame_equal(actual, expected)
