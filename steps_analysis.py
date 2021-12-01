cbbf8702dd91defe6f1596ff7ec744fe0678b1cd
idanacumen <idan@acumen.io>
2021-12-01 15:04:46 +0200

    step analysis tests

--- steps_analysis.py
+++ steps_analysis.py
@@ -23,24 +23,3 @@ def build_two_steps_ds(metric_per_year_df
         )
 
     return joint
-
-if __name__ == "__main__":
-    metric_per_year_df = pd.DataFrame([(2021, 'a', 200, 150)
-                                        , (2020, 'a', 100, 50)
-                                        , (2019, 'a', 50, 50)
-                                        , (2019, 'b', 100, 50)
-                                        , (2018, 'b', 50, 50)
-                                        , (2019, 'c', 100, 50)
-                                        , (2018, 'b', None, 0)
-                                       ]
-                                   , columns=['year', 'repo_name', 'commits', 'authors'])
-    keys=['repo_name']
-    metrics=['commits', 'authors']
-
-    joint = build_two_steps_ds(metric_per_year_df
-                       , keys
-                       , metrics
-                       , time_column='year'
-                       , minimal_time=-1
-                       , control_variables=[])
-    print(joint)
--- test_steps_analysis.py
+++ test_steps_analysis.py
@@ -1,4 +1,3 @@
-import numpy as np
 import pandas as pd
 from pandas.testing import assert_frame_equal
 import pytest
@@ -8,7 +7,6 @@ from steps_analysis import build_two_steps_ds
 @pytest.mark.parametrize(('metric_per_year_df'
                              , 'keys'
                              , 'metrics'
-                                , 'expected'
                             , 'expected')
     , [
 pytest.param(
@@ -19,11 +17,18 @@ pytest.param(
                      , (2019, 'b', 100, 50)
                      , (2018, 'b', 50, 50)
                      , (2019, 'c', 100, 50)
-                     , (2018, 'b', None, 0)
+                     , (2018, 'c', None, 0)
                   ]
         , columns=['year', 'repo_name', 'commits', 'authors'])
-        ,keys= ['repo_name']
-        ,metrics = ['commits', 'authors']
+        ,['repo_name']
+        ,['commits', 'authors']
+        , pd.DataFrame([('a', 200, 150, 2021, 2020, 100, 50, 1.0, 2.0)
+                     , ('a', 100, 50, 2020, 2019, 50, 50, 1.0, 0.0)
+                     , ('b', 100, 50, 2019, 2018, 50, 50, 1.0, 0.0)
+                     , ('c', 100, 50, 2019, 2018, None, 0, None, None)
+                  ]
+        , columns=['repo_name', 'cur_commits', 'cur_authors', 'cur_year', 'prev_year',
+       'prev_commits', 'prev_authors', 'rel_commits', 'rel_authors'])
 
 , id='reg1')
                          ])
@@ -41,7 +46,8 @@ def test_analyze_correlation(metric_per_year_df
                                , control_variables=[])
 
 
-    assert_frame_equal(actual.reset_index(drop=True)
-                       , expected.reset_index(drop=True))
+    assert_frame_equal(actual#.reset_index(drop=True)
+                       , expected#.reset_index(drop=True)
+                       , check_dtype=False  )
 
 
