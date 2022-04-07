b6b0807cb970a421e8391a40b57ca7003761fad2
evidencebp <idanamit@gmail.com>
2022-04-07 11:50:39 +0300

    extract analyze_agreement

--- stability_analysis.py
+++ stability_analysis.py
@@ -25,45 +25,59 @@ def analyze_stability(metric_per_year_df
                        , minimal_time=minimal_time
                        , control_variables=control_variables
                                       )
+
+    all_stats = analyze_agreement(dual_df=two_years_df
+                      , metrics=metrics
+                      , pref_a=CUR_PREFIX
+                      , pref_b=PREV_PREFIX)
+
+    return all_stats
+
+
+def analyze_agreement(dual_df
+                      , metrics
+                      , pref_a='A_'
+                      , pref_b='B_'):
     all_stats = {}
     for i in metrics:
-        cur_metric = CUR_PREFIX + i
-        prev_metric = PREV_PREFIX + i
+        cur_metric = pref_a + i
+        prev_metric = pref_b + i
         stats = {}
-        stats['Pearson'] = two_years_df.corr()[cur_metric][prev_metric]
 
-        two_years_df['diff'] = two_years_df[cur_metric] - two_years_df[prev_metric]
-        stats['diff_avg'] = two_years_df['diff'].mean()
-        stats['diff_std'] = two_years_df['diff'].std()
+        stats['Pearson'] = dual_df.corr()[cur_metric][prev_metric]
 
-        two_years_df['relative_diff'] = two_years_df['diff'].divide(two_years_df[prev_metric])
-        two_years_df.loc[~np.isfinite(two_years_df['relative_diff']), 'relative_diff'] = np.nan
-        stats['relative_diff_avg'] = two_years_df['relative_diff'].mean()
-        stats['relative_diff_std'] = two_years_df['relative_diff'].std()
+        dual_df['diff'] = dual_df[cur_metric] - dual_df[prev_metric]
+        stats['diff_avg'] = dual_df['diff'].mean()
+        stats['diff_std'] = dual_df['diff'].std()
 
+        dual_df['relative_diff'] = dual_df['diff'].divide(dual_df[prev_metric])
+        dual_df.loc[~np.isfinite(dual_df['relative_diff']), 'relative_diff'] = np.nan
+        stats['relative_diff_avg'] = dual_df['relative_diff'].mean()
+        stats['relative_diff_std'] = dual_df['relative_diff'].std()
 
-        two_years_df['abs_diff'] = two_years_df['diff'].map(abs)
-        stats['abs_diff_avg'] = two_years_df['abs_diff'].mean()
-        stats['abs_diff_std'] = two_years_df['abs_diff'].std()
+        dual_df['abs_diff'] = dual_df['diff'].map(abs)
+        stats['abs_diff_avg'] = dual_df['abs_diff'].mean()
+        stats['abs_diff_std'] = dual_df['abs_diff'].std()
 
-        two_years_df['abs_relative_diff'] = two_years_df['abs_diff'].divide(two_years_df[prev_metric])
-        two_years_df.loc[~np.isfinite(two_years_df['abs_relative_diff']), 'abs_relative_diff'] = np.nan
-        stats['abs_relative_diff_avg'] = two_years_df['abs_relative_diff'].mean()
-        stats['abs_relative_diff_avg'] = two_years_df['abs_relative_diff'].std()
+        dual_df['abs_relative_diff'] = dual_df['abs_diff'].divide(dual_df[prev_metric])
+        dual_df.loc[~np.isfinite(dual_df['abs_relative_diff']), 'abs_relative_diff'] = np.nan
+        stats['abs_relative_diff_avg'] = dual_df['abs_relative_diff'].mean()
+        stats['abs_relative_diff_avg'] = dual_df['abs_relative_diff'].std()
 
-        stats['pred_05'] = pred_by_rel_distance(y_test=two_years_df[prev_metric]
-                             , test_pred=two_years_df[cur_metric]
-                             , threshold=0.05)
-        stats['pred_25'] = pred_by_rel_distance(y_test=two_years_df[prev_metric]
-                             , test_pred=two_years_df[cur_metric]
-                             , threshold=0.25)
-        stats['pred_50'] = pred_by_rel_distance(y_test=two_years_df[prev_metric]
-                             , test_pred=two_years_df[cur_metric]
-                             , threshold=0.50)
+        stats['pred_05'] = pred_by_rel_distance(y_test=dual_df[prev_metric]
+                                                , test_pred=dual_df[cur_metric]
+                                                , threshold=0.05)
+        stats['pred_25'] = pred_by_rel_distance(y_test=dual_df[prev_metric]
+                                                , test_pred=dual_df[cur_metric]
+                                                , threshold=0.25)
+        stats['pred_50'] = pred_by_rel_distance(y_test=dual_df[prev_metric]
+                                                , test_pred=dual_df[cur_metric]
+                                                , threshold=0.50)
 
         all_stats[i] = stats.copy()
 
     return all_stats
+
 
 def build_two_years_df(metric_per_year_df
                        , keys
