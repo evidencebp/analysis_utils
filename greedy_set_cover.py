8213f5bbff68042851c74b52071cc29a0be8994a
evidencebp <idanamit@gmail.com>
2022-07-20 15:28:39 +0300

    formatting, typing, link to algorithm

--- greedy_set_cover.py
+++ greedy_set_cover.py
@@ -1,20 +1,26 @@
+"""
+Implementation of the classical greedy set cover algorithm
+https://en.wikipedia.org/wiki/Set_cover_problem#Greedy_algorithm
+"""
 
-def greedy_set_cover(sets_to_cover):
+from typing import List, Set
 
+
+def greedy_set_cover(sets_to_cover: List[Set]):
     covering_items = []
     to_cover = sets_to_cover
     items = set([i for cur_set in sets_to_cover for i in cur_set])
 
-    while (items != [] # Has more items
-            and to_cover != [] # More sets to cover
-            ):
+    while (items != []  # Has more items
+           and to_cover != []  # More sets to cover
+    ):
         # Finding the item that covers most
         most_covering = max(items
-            , key=lambda item: len([cur_set for cur_set in to_cover if item in cur_set]))
+                            , key=lambda item: len([cur_set for cur_set in to_cover if item in cur_set]))
 
         # Bookkeeping
         covering_items.append(most_covering)
         items = items - set([most_covering])
-        to_cover = [cur_set for cur_set in to_cover if not most_covering in cur_set]
+        to_cover = [cur_set for cur_set in to_cover if most_covering not in cur_set]
 
     return covering_items
