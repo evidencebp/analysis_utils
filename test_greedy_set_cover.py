4f9c5c850dd90258c508a0d0c0baf25c933eb1a3
evidencebp <idanamit@gmail.com>
2022-07-20 15:29:23 +0300

    the classical non optimal case, cover all tests

--- test_greedy_set_cover.py
+++ test_greedy_set_cover.py
@@ -13,6 +13,18 @@ pytest.param(
             [set([1]), set([2]), set([3])]
             , [1, 2, 3]
 , id='reg1')
+, pytest.param(
+            [set([1, 4]), set([2, 4]), set([3, 4])]
+            , [4]
+            , id='cover_all')
+, pytest.param(
+            [set([2*i for i in range(7)])
+            , set([1 + 2*i for i in range(7)])
+            , set([0, 7])
+            , set([1, 2, 8, 9])
+            , set([3, 4, 5, 6, 10, 11, 12, 13])]
+            , [0, 1, 3]
+            , id='classic_not_optimal')
                          ])
 def test_greedy_set_cover(sets_to_cover
                              , expected):
