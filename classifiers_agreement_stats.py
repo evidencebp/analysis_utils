02cc68b5c8836062aa493dc505e31fd3795d98fd
evidencebp <idanamit@gmail.com>
2022-12-14 13:50:21 +0200

    classifiers_agreement_stats

---
+++ classifiers_agreement_stats.py
@@ -0,0 +1,36 @@
+from collections import Counter
+from typing import List
+
+import numpy as np
+import pandas as pd
+
+
+def classifiers_agreement_stats(df: pd.DataFrame
+                                , classifiers_columns: List[str] = None
+                                , count_column: str = 'count'):
+
+    # Use all columns but count as default
+    if not classifiers_columns:
+        classifiers_columns = list(set(df.columns) - set([count_column]))
+
+    # Find all values used by the classifiers
+    classifiers_values = sorted(list(np.unique(df[classifiers_columns].values))
+                                , reverse=True)
+
+    rows = []
+    for _, i in df.iterrows():
+        row = []
+        cnt = Counter(i[classifiers_columns])
+        for col in classifiers_values:
+            row.append(cnt[col])
+
+        row.append(i[[count_column]].iloc[0])
+        rows.append(row)
+
+    df = pd.DataFrame(rows
+                      , columns=(classifiers_values + [count_column]))
+    g = df.groupby(classifiers_values
+                   , as_index=False).agg({count_column: 'sum'}).sort_values(classifiers_values
+                                                                            , ascending=False)
+
+    return g.reset_index(drop=True)
---
+++ test_classifiers_agreement_stats.py
@@ -0,0 +1,46 @@
+from typing import List
+
+import pandas as pd
+from pandas.testing import assert_frame_equal
+import pytest
+
+from classifiers_agreement_stats import classifiers_agreement_stats
+
+
+@pytest.mark.parametrize(('df'
+                            , 'classifiers_columns'
+                            , 'count_column'
+                            , 'expected')
+    , [
+pytest.param(
+            pd.DataFrame([
+                [1, 1, 1, 100]
+                , [1, 0, 1, 20]
+                , [1, 1, 0, 30]
+                , [-1, 1, 1, 32]
+                , [-1, -1, -1, 1000]
+                ], columns=['f1', 'f2', 'f3', 'count'])
+            , ['f1', 'f2', 'f3']
+            , 'count'
+            , pd.DataFrame([
+                [3, 0, 0, 100]
+                , [2, 1, 0, 50]
+                , [2, 0, 1, 32]
+                , [0, 0, 3, 1000]
+    ], columns=[1, 0, -1, 'count'])
+
+, id='reg1')
+                         ])
+def test_classifiers_agreement_stats(df: pd.DataFrame
+                                , classifiers_columns: List[str]
+                                , count_column: str
+                                , expected):
+    actual = classifiers_agreement_stats(df=df
+                                , classifiers_columns=classifiers_columns
+                                , count_column=count_column)
+
+
+    assert_frame_equal(actual.reset_index(drop=True)
+                       , expected.reset_index(drop=True))
+
+
