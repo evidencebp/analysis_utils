e92793526d831bbaebd2aaab3d75c5531f4ad902
Michael Pinus <michaelpinus@moodify.ai>
2024-05-09 15:44:07 +0300

    require cloudpickle

--- setup.py
+++ setup.py
@@ -1,13 +1,14 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='analysis_utils',
-    version='1.0',
-    packages=find_packages(),
-    install_requires=[
-        'pandas',
-        'psutil',
-        'plotly',
-        'scikit-learn',
-        ],
-    )
+        name='analysis_utils',
+        version='1.0',
+        packages=find_packages(),
+        install_requires=[
+            'pandas',
+            'psutil',
+            'plotly',
+            'scikit-learn',
+            'cloudpickle'
+            ],
+        )
