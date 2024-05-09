f703478aa96999ad104be6b7e70526a9cfc5fba3
Michael Pinus <michaelpinus@moodify.ai>
2024-05-09 15:26:41 +0300

    create setup.py file

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='analysis_utils',
+    version='1.0',
+    packages=find_packages(),
+    install_requires=[
+        'pandas',
+        'psutil',
+        'plotly-orca',
+        'scikit-learn',
+        ],
+    )
