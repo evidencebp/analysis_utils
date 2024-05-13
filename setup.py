64e1d85bf431009f3b76b6232ccd603b220112f5
Michael Pinus <michaelpinus@moodify.ai>
2024-05-13 13:58:05 +0300

    add readme and repo url to description

--- setup.py
+++ setup.py
@@ -1,8 +1,14 @@
 from setuptools import setup, find_packages
 
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
 setup(
         name='analysis_utils',
         version='1.0',
+        long_description=long_description,
+        long_description_content_type="text/markdown",
+        url="https://github.com/evidencebp/analysis_utils",
         packages=find_packages(),
         install_requires=[
             'pandas',
@@ -11,4 +17,8 @@ setup(
             'scikit-learn',
             'cloudpickle'
             ],
+        classifiers=[
+            "Programming Language :: Python :: 3",
+            "Operating System :: OS Independent",
+            ]
         )
