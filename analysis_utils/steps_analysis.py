018a773d2b805ad6f3a59f956bce94273961cba1
evidencebp <idanamit@gmail.com>
2024-05-15 07:47:42 +0300

    Extend interface

--- analysis_utils/steps_analysis.py
+++ analysis_utils/steps_analysis.py
@@ -5,14 +5,17 @@ from analysis_utils.stability_analysis import build_two_years_df
 def build_two_steps_ds(metric_per_year_df
                        , keys
                        , metrics
-                       , time_column='year'):
+                       , time_column='year'
+                       , minimal_time=-1
+                       , control_variables=[]
+                       ):
 
     joint = build_two_years_df(metric_per_year_df
-                       , keys
-                       , metrics
-                       , time_column=time_column
-                       , minimal_time=-1
-                       , control_variables=[])
+                                , keys
+                                , metrics
+                                , time_column=time_column
+                                , minimal_time=minimal_time
+                                , control_variables=control_variables)
 
     for i in metrics:
         joint['rel_'+ i] = joint.apply(lambda x: None if (x['prev_'+ i] is None or x['prev_'+ i] ==0) else
